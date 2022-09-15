da8497d45b49b86b13e566a774e4ea6d39a8ba92
Man Cheng <cyrilcheng@protonmail.com>
2022-09-15 14:12:58 -0700

    commit tree 3e657611cd96e953b5540e8dd949c4abee0380b0

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Add your code here
+
+
+const submitData = function(userName, userEmail){
+ const userInfo = {
+    name: userName,
+    email: userEmail
+ };
+try {
+        return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: { 
+            "Content-Type" : "application/json",
+            "Accept" : "application/json"
+            },
+        body: JSON.stringify(userInfo)
+        }).then((res)=> {return res.json();
+        }).then((jsonData)=> {return document.body.innerHTML =jsonData.id
+        }).catch((err) => {document.body.innerHTML = err.message;
+        });
+
+    } catch(err) {
+    console.log("Error fetching data from servers");
+    }
+}
+submitData("Cyril", "cyril@protonmail.com");
